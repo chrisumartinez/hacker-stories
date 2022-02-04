14a2003dacba8d4de501b8394394101bff792073
chrisumartinez <chrisumartinez@gmail.com>
2022-02-03 17:18:32 -0800

    introduced JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const title = "React";
+
+const App = () => {
 	return (
-		<div className="App">
-			<h1>Hello World!</h1>
+		<div>
+			<h1>Hello {title}</h1>
+			<label htmlFor="search">Search: </label>
+			<input id="search" type="text" />
 		</div>
 	);
-}
+};
 
 export default App;
