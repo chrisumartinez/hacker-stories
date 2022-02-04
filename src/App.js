84cc627f17e778c211ff52fd18b20e533abf228f
chrisumartinez <chrisumartinez@gmail.com>
2022-02-03 19:23:19 -0800

    used to props to access list from one component to another

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
 import "./App.css";
 
-const list = [
-	{
-		title: "React",
-		url: "https://reactjs.org/",
-		author: "Jordan Walke",
-		num_comments: 3,
-		points: 4,
-		objectID: 0,
-	},
-	{
-		title: "Redux",
-		url: "https://redux.js.org/",
-		author: "Dan Abramov, Andrew Clark",
-		num_comments: 2,
-		points: 5,
-		objectID: 1,
-	},
-];
-
-const List = () => {
+const List = ({ list }) => {
 	return list.map((item) => {
 		return (
 			<div key={item.objectID}>
@@ -35,6 +16,25 @@ const List = () => {
 };
 
 const App = () => {
+	const stories = [
+		{
+			title: "React",
+			url: "https://reactjs.org/",
+			author: "Jordan Walke",
+			num_comments: 3,
+			points: 4,
+			objectID: 0,
+		},
+		{
+			title: "Redux",
+			url: "https://redux.js.org/",
+			author: "Dan Abramov, Andrew Clark",
+			num_comments: 2,
+			points: 5,
+			objectID: 1,
+		},
+	];
+
 	const handleChange = (event) => {
 		console.log(event.target.value);
 	};
@@ -46,7 +46,7 @@ const App = () => {
 			<input id="search" type="text" onChange={handleChange} />
 			<hr />
 			{/* Render List Below */}
-			<List />
+			<List list={stories} />
 		</div>
 	);
 };
